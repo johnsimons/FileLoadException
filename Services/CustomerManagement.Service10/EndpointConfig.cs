d8c5430454b892ab8d7de5c6b5de2268b7b73d13
John Simons <johnsimons007@yahoo.com.au>
2012-07-10 12:30:51 +1000

    Project that replicates https://github.com/NServiceBus/NServiceBus/issues/59

---
+++ Services/CustomerManagement.Service10/EndpointConfig.cs
@@ -0,0 +1,30 @@
+﻿using NServiceBus;
+
+namespace Wwa.Lms.Services.CustomerManagement.Service10
+{
+    using System.Reflection;
+    /*
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Publisher, IWantCustomInitialization
+    {
+        public void Init()
+        {
+            Configure.With()
+                .DefaultBuilder()
+                .DefineEndpointName(Assembly.GetExecutingAssembly().GetName().Name)
+                .XmlSerializer()
+                .MsmqTransport()
+                .IsTransactional(true)
+                .UnicastBus()
+                .ImpersonateSender(false);
+        }
+    }
+    */
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Server, IWantCustomInitialization
+    {
+        public void Init()
+        {
+            Configure.With()
+                .DefineEndpointName(Assembly.GetExecutingAssembly().GetName().Name);
+        }
+    }
+}
